d3e53ccf585274adf2ae4d99294e88dc66e3abf9
Gopal Verma <iasgopalverma9621@gmail.com>
2026-01-29 00:33:25 +0530

    Add files via upload

---
+++ DSA Assesment 1 practice/AvgEleArray.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+public class AvgEleArray {
+   public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int avg = 0;
+    int[] arr = new int[n];
+    for(int i= 0;i<n;i++){
+        arr[i] = sc.nextInt();
+         avg = avg + arr[i] ;
+        }
+         System.out.println(avg/n);
+    }
+   } 
+
+
+    
+
